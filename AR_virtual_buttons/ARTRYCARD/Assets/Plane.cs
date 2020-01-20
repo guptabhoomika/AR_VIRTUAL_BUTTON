08ba18e1b4ecd612d73a3f48b11c2136411c1658
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-01-20 17:55:55 +0530

    commit1

---
+++ AR_virtual_buttons/ARTRYCARD/Assets/Plane.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Plane : MonoBehaviour, IVirtualButtonEventHandler
+{
+   public GameObject plane1, plane3;
+    // Start is called before the first frame update
+ 
+
+    void Start()
+    {
+        VirtualButtonBehaviour[] vrb = GetComponentsInChildren<VirtualButtonBehaviour>();
+   
+       
+        for (int i= 0; i < vrb.Length; i++)
+        {
+            vrb[i].RegisterEventHandler(this);
+
+        }
+        plane1.SetActive(false);
+        
+        plane3.SetActive(false);
+        
+
+
+
+        
+    }
+   
+   
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if(vb.VirtualButtonName == "vb1")
+        {
+            Debug.Log("VB1");
+            plane1.SetActive(true);
+            plane3.SetActive(false);
+           
+          
+        }
+       
+       else if (vb.VirtualButtonName == "vb3")
+        {
+            Debug.Log("VB3");
+            plane3.SetActive(true);
+            plane1.SetActive(false);
+           
+        }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName + "Virtual button not supported");
+        }
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Released");
+        
+    }
+}
